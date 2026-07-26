b0d20e2b34bd5734f5e9d3ea4d4b5cf096e885cd
Tanmay Melanta <tanmaymelanta@gmail.com>
2026-07-26 11:23:19 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,41 +1,23 @@
-from playwright.sync_api import sync_playwright
+import requests
 import pandas as pd
 
-URL = "https://grid-india.in/en/reports/daily-psp-report"
-print(URL)
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=True, args=["--no-sandbox"])
-    print(f"browser {browser}")
-    page = browser.new_page(viewport={"width": 1920, "height": 1080})
-    print(f"page {page}")
-    page.goto(URL, wait_until="networkidle")
-    print("page goto url")
-    # Wait until page loads
-    page.wait_for_selector("table")
+url = "https://webapi.grid-india.in/api/v1/file"
 
-    # Click Financial Year dropdown
-    page.locator("div[role='button']").first.click()
+payload = {
+    "_source": "GRDW",
+    "_type": "DAILY_PSP_REPORT",
+    "_fileDate": "2013-14",
+    "_month": "00"
+}
 
-    # Select 2013-14
-    page.get_by_text("2013-14", exact=True).click()
+response = requests.post(url, json=payload)
 
-    # Wait for table refresh
-    page.wait_for_load_state("networkidle")
-    page.wait_for_timeout(3000)
+response.raise_for_status()
 
-    table = page.locator("table")
+data = response.json()["retData"]
 
-    headers = table.locator("thead th").all_inner_texts()
+df = pd.DataFrame(data)
 
-    rows = []
-
-    for row in table.locator("tbody tr").all():
-        rows.append(row.locator("td").all_inner_texts())
-
-    browser.close()
-
-df = pd.DataFrame(rows, columns=headers)
-
-print(df)
+print(df.head())
 
 df.to_csv("daily_psp_2013_14.csv", index=False)
