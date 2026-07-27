123c6354e099401ae84d81d76fc5a61d5c9f541b
Tanmay Melanta <tanmaymelanta@gmail.com>
2026-07-27 15:48:50 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -7,7 +7,11 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 options = Options()
-options.add_argument("--start-maximized")
+options.add_argument("--headless=new")   # or --headless
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--window-size=1920,1080")
 driver = webdriver.Chrome(options=options)
 
 driver.get("https://grid-india.in/en/reports/daily-psp-report")
