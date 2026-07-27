44f3b5e0bd3a0fee6779630bcb2e31d7206af84e
Tanmay Melanta <tanmaymelanta@gmail.com>
2026-07-27 16:02:13 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -7,11 +7,13 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 options = Options()
-options.add_argument("--headless=new")   # or --headless
+options.add_argument("--headless=new")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
 options.add_argument("--window-size=1920,1080")
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36")
 driver = webdriver.Chrome(options=options)
 
 driver.get("https://grid-india.in/en/reports/daily-psp-report")
